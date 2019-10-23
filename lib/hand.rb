05fe82b95afb64d1f98d191fe581074eceeded20
J Resilient <j.resilient.1@gmail.com>
2019-10-23 11:50:05 -0400

    Refactor to properly handle mixed high-low straights

--- lib/hand.rb
+++ lib/hand.rb
@@ -25,33 +25,86 @@ class Hand
         :high_card
     end
 
+    # def <=>(hand2)
+    #     unless self.hand_rank == hand2.hand_rank
+    #         return HAND_POINTS[self.hand_rank] <=> HAND_POINTS[hand2.hand_rank]
+    #     end
+
+    #     # nil is falsy: everything else is truthy
+    #     return 0 if self.hand.all? do |card|
+    #         hand2.hand.find { |card2| card.suit == card2.suit && card.value == card2.value }
+    #     end
+
+    #     case self.hand_rank
+    #     when :straight_flush, :straight
+    #         return compare_straights
+    #     end
+    #     nil
+    # end
+
     private
-    VALUES = {
-        :ace => 0, :king => 1, :queen => 2, :jack => 3,
-        :ten => 4, :nine => 5, :eight => 6, :seven => 7, :six => 8, 
-        :five => 9, :four => 10, :three => 11, :two => 12
+    HAND_POINTS = {
+        :straight_flush => 8,
+        :four_kind => 7,
+        :full_house => 6,
+        :flush => 5,
+        :straight => 4,
+        :three_kind => 3,
+        :two_pair => 2,
+        :one_pair => 1,
+        :high_card => 0
     }
+    CARD_POINTS = {
+        :ace => 14,
+        :king => 13,
+        :queen => 12,
+        :jack => 11,
+        :ten => 10,
+        :nine => 9,
+        :eight => 8,
+        :seven => 7,
+        :six => 6,
+        :five => 5,
+        :four => 4,
+        :three => 3,
+        :two => 2,
+        :ace_low => 1
+    }
+
+    def get_sorted_points(hand)
+        points = []
+        hand.each do |card| 
+            if card.value == :ace && hand.none? { |card| card.value == :king }
+                points << CARD_POINTS[:ace_low]
+            else
+                points << CARD_POINTS[card.value]
+            end
+        end
+        points.sort!
+    end
 
     def straight_flush?
         straight? && flush?
     end
 
     def straight?
-        hand_numbers = []
-        hand.each { |card| hand_numbers << VALUES[card.value] }
-        hand_numbers.sort!
+        hand_points = get_sorted_points(self.hand)
 
-        if hand_numbers.first == 0 && hand_numbers.last == 12
-            hand_numbers.shift
-            hand_numbers.push(13)
-        end
-        
-        prev = hand_numbers.first
-        hand_numbers[1..-1].each do |num|
-            return false unless num == (prev + 1)
-            prev = num
+        if mixed_high_low?(hand_points)
+            remaining_points = hand_points.reject { |point| [2, 14, 13].include?(point) }
+            return false unless [[3,4], [3, 12], [11, 12]].include?(remaining_points)
+        else
+            prev = hand_points.first
+            hand_points[1..-1].each do |point|
+                return false unless point == (prev + 1)
+                prev = point
+            end
         end
         true
+    end
+
+    def mixed_high_low?(points)
+        points.include?(14) && points.include?(13) && points.include?(2)
     end
 
     def flush?
@@ -83,13 +136,19 @@ class Hand
     def one_pair?
         same_card_count.include?(2)
     end
+
+    # def compare_straights(hand2)
+    #     # find high cards
+    #     # compare high cards
+    #     self_high_card = find_high_card(self.hand)
+    #     hand2_high_card = find_high_card(hand2)
+
+    #     self_points = CARD_POINTS[self_high_card] 
+    #     hand2_points = CARD_POINTS[hand2_high_card]
+
+    #     # if self_high_card == :ace && self.hand.any? { |card| card.value == :two }
+    #     #     self_points
+    #     # end
+    # end
 end
 
-# x = Hand.new([
-#             Card.new(:club, :five),
-#             Card.new(:diamond, :five),
-#             Card.new(:heart, :five),
-#             Card.new(:spade, :five),
-#             Card.new(:diamond, :two)
-#         ])
-# print x.hand_rank
