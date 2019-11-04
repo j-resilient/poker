58aca1d45a141d717a78328f8ef2abb384ec8e2a
J Resilient <j.resilient.1@gmail.com>
2019-11-04 13:27:59 -0500

    Add tests for player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,69 @@
+require 'player'
+
+describe 'Player' do
+    describe '::buy_in' do
+        it 'creates a player' do
+            expect(Player.buy_in(100)).to be_a(Player)
+        end
+
+        it 'sets pot' do
+            expect(Player.buy_in(100).pot).to eq(100)
+        end
+    end
+
+    describe '#set_hand' do
+        let(:hand) { double ('hand') }
+
+        it 'sets the players hand' do
+            player.set_hand(hand)
+            expect(player.hand).to eq(hand)
+        end
+    end
+
+    subject(:player_one) { Player.buy_in(100) 
+}
+    describe '#bet' do
+        it 'decrements the pot by correct amount' do
+            expect(player_one.bet(50)).to change { player_one.pot }.by(-50)
+        end
+        it 'raises an error if bet is more than the pot' do
+            expect(player_one.bet(200)).to raise_error("Cannot bet more than you have.")
+        end
+        it 'returns the bet amount' do
+            expect(player_one.bet(50)).to eq(50)
+        end
+    end
+
+    describe '#add_winnings' do
+        it 'adds winnings to pot' do
+            expect(player_one.add_winnings(100)).to change { player_one.pot }.by(100)
+        end
+    end
+
+    describe '#return cards' do
+        let(:hand) { double('Hand', :cards => double('Cards')) }
+        before(:each) { player_one.set_hand(hand) }
+        it 'returns cards' do
+            expect(player_one.return_cards).to eq(cards)
+        end
+        it 'sets hand to nil' do
+            player_one.return_cards
+            expect(player_one.hand).to eq(nil)
+        end
+    end
+
+    describe '#fold' do
+        it 'sets folded? to true' do
+            player_one.fold
+            expect(player_one).to be_folded
+        end
+    end
+
+    describe '#unfold' do
+        it 'sets folded? to false' do
+            player_one.fold
+            player_one.unfold
+            expect(player_one).to_not be_folded
+        end
+    end
+end
